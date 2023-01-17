76fbdf3e34bf1077d0cc646a40499ae8e76d42c7
Oluwadabira <adigunsamuelaso@gmail.com>
2023-01-17 12:52:18 +0100

    definition of player object

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const gameContainer = document.querySelector('#game-container');
 const cells = document.querySelectorAll('.cell');
 const restartButton = document.querySelector('#restart');
+
+let player = function (name, marker) {
+	return { name, marker };
+};
