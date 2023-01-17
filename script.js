5c3935d6d923f6e3bc7abe1f35b5dbd0705a4525
Oluwadabira <adigunsamuelaso@gmail.com>
2023-01-17 12:55:28 +0100

    definition of both players

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ const restartButton = document.querySelector('#restart');
 let player = function (name, marker) {
 	return { name, marker };
 };
+let player1 = player('player1', 'X');
+let player2 = player('player2', 'O');
